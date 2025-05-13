94655bea84b2969a03ca97b1963b2615483af116
Maimas2 <aseltz156@gmail.com>
2025-05-13 09:32:14 -0400

    Fix, please work

--- mun/index.js
+++ mun/index.js
@@ -111,6 +111,7 @@ app.post("/savesavedata", jsonParse, (req, res) => {
         code    : 201,
         message : "Save data successfully stored"
     }));
+    console.log(savedSaveData);
 });
 
 app.post("/getsavedata", jsonParse, (req, res) => {
