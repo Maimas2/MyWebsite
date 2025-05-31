167c7fce0804f026db9d03b87914154049ba0f61
Maimas2 <aseltz156@gmail.com>
2025-05-31 18:36:14 -0400

    Added todo list

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require("express");
-const vhost   = require("vhost");
+const express    = require("express");
+const vhost      = require("vhost");
+const fs         = require("fs");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.port || 3000;
@@ -15,6 +17,11 @@ var sbestApp = sbestFile.app;
 
 var namesFile = require("./names");
 app.use(namesFile.app);
+
+var textParse = bodyParser.text();
+app.use(textParse);
+
+var todoList = "";
 
 process.on("SIGTERM", receivedKillSignal);
 process.on("SIGINT",  receivedKillSignal);
@@ -41,6 +48,16 @@ app.get("/lib/jquery.js", (req, res) => {
     res.sendFile("./mun/lib/jquery-3.7.1.min.js", {root: __dirname});
 });
 
+app.post("/settodo", textParse, (req, res) => {
+    todoList = req.body;
+    //console.log(req.body);
+    res.status(200).send("Set todo list");
+});
+
+app.get("/gettodo", (req, res) => {
+    res.send(todoList);
+});
+
 app.use(function(req, res, next) {
     res.send("404 couldn't find that page :(");
 });
@@ -53,6 +70,14 @@ app.listen(port, () => {
     munFile.startUpFunction();
     scibowlFile.startUpFunction();
     sbestFile.startUpFunction();
+
+    if(fs.existsSync("./saves/todo_list.txt")) {
+        var d = fs.readFileSync("./saves/todo_list.txt", "utf-8");
+        console.log(d);
+        todoList = d;
+    } else {
+        console.warn("Could not find Todo list file!!!");
+    }
 });
 
 app.listen(3001, () => {
@@ -63,6 +88,10 @@ function receivedKillSignal() {
     console.log("Shutting down...");
     munFile.shutDownFunction();
     sbestFile.shutDownFunction();
+
+    fs.writeFileSync("./saves/todo_list.txt", todoList, "utf-8", (error) => {
+        if(error) console.log(error);
+    });
 
     process.exit(0);
 }
