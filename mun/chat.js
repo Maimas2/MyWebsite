a5e0a3bc2af335d8357a46765e6caa88563be1d7
Maimas2 <aseltz156@gmail.com>
2025-06-06 17:20:58 -0400

    Oh my god I hate this

--- mun/chat.js
+++ mun/chat.js
@@ -33,7 +33,7 @@ function createMessage(data, isFromSelf=false) {
 }
 
 window.onload = function() {
-    wsUrl = window.location.toString().includes("localhost") ? "ws://mun.localhost:3002" : "wss://" + window.location.host + ":3002";
+    wsUrl = window.location.toString().includes("localhost") ? "ws://mun.localhost:3002/ws" : "wss://" + window.location.host + ":3002/ws";
     $("#join").on("click", function(_e) {
         var d = {
             name     : $("#jccName").val(),
