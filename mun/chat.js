cc160e7a6b7b159238fc63a49f2be90927c09824
Maimas2 <aseltz156@gmail.com>
2025-06-06 19:37:18 -0400

    Take number 673958

--- mun/chat.js
+++ mun/chat.js
@@ -33,7 +33,7 @@ function createMessage(data, isFromSelf=false) {
 }
 
 window.onload = function() {
-    wsUrl = window.location.toString().includes("localhost") ? "ws://mun.localhost:2053/" : "wss://" + window.location.host + ":2053/";
+    wsUrl = window.location.toString().includes("localhost") ? "ws://mun.localhost/" : "wss://" + window.location.host + "/";
     $("#join").on("click", function(_e) {
         var d = {
             name     : $("#jccName").val(),
--- mun/index.js
+++ mun/index.js
@@ -350,8 +350,8 @@ app.post("/getsavedata", jsonParse, (req, res) => {
     }
 });
 
-app.listen(2053, () => {
-	console.log(`MunWS is listening on port ${2053}`);
+app.listen(3000, () => {
+	console.log(`MunWS is listening on port ${3000}`);
 });
 
 module.exports.app = app;
