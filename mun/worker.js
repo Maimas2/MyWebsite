3c84b31b0efaaf9bb1a5ca76e5a6ce7c5b1f5e6d
Maimas2 <aseltz156@gmail.com>
2025-06-05 22:54:33 -0400

    Take 2

--- mun/worker.js
+++ mun/worker.js
@@ -1350,7 +1350,7 @@ function setupJccData(data) {
     jccData = data;
     $("#jccNameSpan").text(data.name);
 
-    ws = new WebSocket("ws://mun.localhost:3002", "echo-protocol");
+    ws = new WebSocket("ws://" + window.location.host, "echo-protocol");
     
     ws.addEventListener("open", function(_e) {
         ws.send(JSON.stringify({
