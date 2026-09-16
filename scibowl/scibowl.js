4a09db6f42031c4b8c7974eb75475a149e43b8e6
alex <aseltz156@gmail.com>
2026-09-16 18:36:10 -0400

    Scibowl fixes

--- scibowl/scibowl.js
+++ scibowl/scibowl.js
@@ -4,31 +4,31 @@ var penaltiesGoToOpposite = true;
 
 $(document).ready(function() { // Soooon......
     $("#shareGame").on("click", function() {
-        
+
         var d = {
-            name     : prompt("Name"),
-            password : prompt("Password")
+            name: prompt("Name"),
+            password: prompt("Password")
         };
-        if(d.name == null)
-        hidePopup();
+        if (d.name == null)
+            hidePopup();
         $("#startJCC").css("display", "none");
         $.ajax({
-            type    : "POST",
-            url     : "/createJCC",
+            type: "POST",
+            url: "/createJCC",
             contentType: 'application/json',
-            success : function(returned) {
+            success: function(returned) {
                 console.log(returned);
 
                 setupJccData(returned);
             },
-            error   : function(returned) {
+            error: function(returned) {
                 console.error(JSON.parse(returned.responseText));
                 createAlert(JSON.parse(returned.responseText).message);
                 $("#startJCC").css("display", "inline-block");
             },
-            data    : JSON.stringify(d)
+            data: JSON.stringify(d)
         });
-    })
+    });
 });
 
 
@@ -40,28 +40,28 @@ function updateScores() {
 
     let latestClicked = 0;
 
-    for(let ii = 0; ii < numQuestions; ii++) {
-        let i = ii*2;
+    for (let ii = 0; ii < numQuestions; ii++) {
+        let i = ii * 2;
         $(tLeftArray[i]).children("button").each(function() {
-            if($(this).attr("data-clicked") == "true") {
+            if ($(this).attr("data-clicked") == "true") {
                 latestClicked = ii;
             }
         });
         $(tRightArray[i]).children("button").each(function() {
-            if($(this).attr("data-clicked") == "true") {
+            if ($(this).attr("data-clicked") == "true") {
                 latestClicked = ii;
             }
         });
     }
 
-    for(let ii = 0; ii < numQuestions; ii++) {
-        let i = ii*2;
+    for (let ii = 0; ii < numQuestions; ii++) {
+        let i = ii * 2;
         let clickedArray = [];
 
         $(tLeftArray[i]).children("button").each(function() {
             clickedArray.push($(this).attr("data-clicked") == "true");
-            if($(this).attr("data-clicked") == "true") {
-                if(scores[Number($(this).attr("data-pointid"))] > 0 || !$("#penaltySide").prop("checked")) {
+            if ($(this).attr("data-clicked") == "true") {
+                if (scores[Number($(this).attr("data-pointid"))] > 0 || !$("#penaltySide").prop("checked")) {
                     lscore += scores[Number($(this).attr("data-pointid"))];
                 } else {
                     rscore -= scores[Number($(this).attr("data-pointid"))];
@@ -70,8 +70,8 @@ function updateScores() {
         });
         $(tRightArray[i]).children("button").each(function() {
             clickedArray.push($(this).attr("data-clicked") == "true");
-            if($(this).attr("data-clicked") == "true") {
-                if(scores[Number($(this).attr("data-pointid"))] > 0 || !$("#penaltySide").prop("checked")) {
+            if ($(this).attr("data-clicked") == "true") {
+                if (scores[Number($(this).attr("data-pointid"))] > 0 || !$("#penaltySide").prop("checked")) {
                     rscore += scores[Number($(this).attr("data-pointid"))];
                 } else {
                     lscore -= scores[Number($(this).attr("data-pointid"))];
@@ -80,18 +80,18 @@ function updateScores() {
         });
 
         $(tLeftArray[i]).children("button").each(function(i) {
-            if(i == 0) {
+            if (i == 0) {
                 $(this).attr("disabled", clickedArray[1] || clickedArray[2]);
             }
-            if(i == 1 || i == 2) {
+            if (i == 1 || i == 2) {
                 $(this).attr("disabled", clickedArray[4] || clickedArray[5] || clickedArray[0]);
             }
         });
         $(tRightArray[i]).children("button").each(function(i) {
-            if(i == 0) {
+            if (i == 0) {
                 $(this).attr("disabled", clickedArray[4] || clickedArray[5]);
             }
-            if(i == 1 || i == 2) {
+            if (i == 1 || i == 2) {
                 $(this).attr("disabled", clickedArray[1] || clickedArray[2] || clickedArray[3]);
             }
         });
@@ -99,13 +99,13 @@ function updateScores() {
         $($(tLeftArray[i]).children("p").get(1)).text(lscore);
         $($(tRightArray[i]).children("p").get(0)).text(rscore);
 
-        if(lscore > rscore && ii <= latestClicked) {
-            $($(tLeftArray[i]).children("p").get(1)).css("background-color", "rgb(100, 255, 100)");
+        if (lscore > rscore && ii <= latestClicked) {
+            $($(tLeftArray[i]).children("p").get(1)).css("background-color", "rgb(150, 255, 150)");
         } else {
             $($(tLeftArray[i]).children("p").get(1)).css("background-color", "");
         }
-        if(lscore < rscore && ii <= latestClicked) {
-            $($(tRightArray[i]).children("p").get(0)).css("background-color", "rgb(100, 255, 100)");
+        if (lscore < rscore && ii <= latestClicked) {
+            $($(tRightArray[i]).children("p").get(0)).css("background-color", "rgb(150, 255, 150)");
         } else {
             $($(tRightArray[i]).children("p").get(0)).css("background-color", "");
         }
@@ -113,12 +113,12 @@ function updateScores() {
     $($("#leftScore").children("p").get(0)).text(lscore);
     $($("#rightScore").children("p").get(0)).text(rscore);
 
-    if(lscore > rscore) {
+    if (lscore > rscore) {
         $("#leftScore").css("background-color", "rgb(100, 255, 100)");
     } else {
         $("#leftScore").css("background-color", "");
     }
-    if(lscore < rscore) {
+    if (lscore < rscore) {
         $("#rightScore").css("background-color", "rgb(100, 255, 100)");
     } else {
         $("#rightScore").css("background-color", "");
@@ -139,23 +139,23 @@ function setup() {
 
     var t = $("#tempDuplicate").clone(true).attr("id", "");
     var tr = $("#tempDuplicateRight").clone(true).attr("id", "");
-    for(let i = 0; i < numQuestions; i++) {
+    for (let i = 0; i < numQuestions; i++) {
         let tt = t.clone();
-        $(tt.children("p").get(0)).text(`Q.${i+1}`);
+        $(tt.children("p").get(0)).text(`Q.${i + 1}`);
         tt.children("button").attr("data-clicked", false);
 
         tt.children("button")[0].textContent = scores[0];
         tt.children("button")[1].textContent = "+" + scores[1];
         tt.children("button")[2].textContent = "+" + scores[2];
 
-        tt.css("border-right", "1px solid #aaa");
+        tt.addClass("leftScoreHolder");
 
         $("#leftButtons").append(tt);
-        if(i != numQuestions-1) $("#leftButtons").append($("<div>").addClass("verticalDivider"));
+        if (i != numQuestions - 1) $("#leftButtons").append($("<div>").addClass("verticalDivider"));
     }
-    for(let i = 0; i < numQuestions; i++) {
+    for (let i = 0; i < numQuestions; i++) {
         let tt = tr.clone();
-        $(tt.children("p").get(1)).text(`Q.${i+1}`);
+        $(tt.children("p").get(1)).text(`Q.${i + 1}`);
         tt.children("button").attr("data-clicked", false);
 
         tt.children("button")[0].textContent = scores[0];
@@ -163,27 +163,27 @@ function setup() {
         tt.children("button")[2].textContent = "+" + scores[2];
 
         $("#rightButtons").append(tt);
-        if(i != numQuestions-1) $("#rightButtons").append($("<div>").addClass("verticalDivider"));
+        if (i != numQuestions - 1) $("#rightButtons").append($("<div>").addClass("verticalDivider"));
     }
 
     $(".scoreButton").mousedown(function(e) {
-        if(e.button == 0 && $(this).attr("data-disabled") != "true") {
-            if($(this).attr("data-clicked") == "true") {
+        if (e.button == 0 && $(this).attr("data-disabled") != "true") {
+            if ($(this).attr("data-clicked") == "true") {
                 $(this).attr("data-clicked", "false");
                 $(this).css("background-color", "");
             } else {
                 $(this).attr("data-clicked", "true");
                 $(this).css("background-color", $(this).attr("data-bg"));
-                if($(this).attr("data-pointid") == 2) {
-                    if($(this).prev().attr("data-clicked") != "true") {
+                if ($(this).attr("data-pointid") == 2) {
+                    if ($(this).prev().attr("data-clicked") != "true") {
                         let e = new MouseEvent("mousedown");
                         $(this).prev().get(0).dispatchEvent(e);
                     }
                 }
             }
-        } else if(e.button == 2) {
+        } else if (e.button == 2) {
             $(this).attr("data-clicked", "false");
-            if($(this).attr("data-disabled") == "true") {
+            if ($(this).attr("data-disabled") == "true") {
                 $(this).attr("data-disabled", false);
                 $(this).attr("disabled", false);
                 $(this).css("background-color", "");
@@ -201,19 +201,21 @@ function setup() {
         updateScores();
     });
 
+    document.documentElement.style.setProperty("--number-buttons", numQuestions);
+
     $("#blackout, #settingsPopup").css("display", "none");
 }
 
 $(".addButton").on("click", function() {
-    $(this).prev().val(Number($(this).prev().val())+1);
-    if(Number($(this).prev().val()) > Number($(this).prev().prop("max"))) {
+    $(this).prev().val(Number($(this).prev().val()) + 1);
+    if (Number($(this).prev().val()) > Number($(this).prev().prop("max"))) {
         $(this).prev().val(Number($(this).prev().prop("max")));
     }
 });
 
 $(".subtractButton").on("click", function() {
-    $(this).next().val(Number($(this).next().val())-1);
-    if(Number($(this).next().val()) < Number($(this).next().prop("min"))) {
+    $(this).next().val(Number($(this).next().val()) - 1);
+    if (Number($(this).next().val()) < Number($(this).next().prop("min"))) {
         $(this).next().val(Number($(this).next().prop("min")));
     }
 });
